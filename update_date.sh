d492918bbbf8fe6e9fc83540570e90f4780164cc
Jungtaek Kim <jtkim@postech.ac.kr>
2022-02-11 14:31:08 +0900

    Update all

--- update_date.sh
+++ update_date.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git_last_commit_date="$(git log -1 --format=%cd)"
-echo "Git last commit date: $git_last_commit_date"
+str_now=`date +'%B %-d, %Y'`
+echo $str_now
 
 html_files="$(find . -type f -name "*.html")"
 
 for file in $html_files; do 
     echo "Inserting date into: $file" 
-    sed -i "" "s|<p class=\"paragraph-100\" id=\"last-updated\">*</p>|<p class=\"paragraph-100\" id=\"last-updated\">Last updated: $git_last_commit_date</p>|g" "$file"
+    sed -i "s|<p class=\"paragraph-100\" id=\"last-updated\">.*</p>|<p class=\"paragraph-100\" id=\"last-updated\">Last updated: $str_now</p>|g" $file
 done 
